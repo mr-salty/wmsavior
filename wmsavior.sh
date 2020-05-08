c13c7615d583efc5322bbbfdfafabddc9762d090
Todd Derr <salty@google.com>
2020-05-08 13:49:38 -0400

    checkpoint wmsavior - added a bunch of new stuff (backups, diff, etc)

--- wmsavior.sh
+++ wmsavior.sh
@@ -20,26 +20,70 @@
 # you can use the script to restore windows arrangement.
 # Created by dosmanak.
 
-STORFILE="$(dirname $(realpath $0))/saved.wmctrl"
+# store files in this directory; try to create it if necessary
+readonly DIR="${HOME}/.wmsavior";
+if ! cd "${DIR}"; then
+  mkdir -p ${DIR}
+  if ! cd "${DIR}"; then
+    echo "Can't chdir to ${DIR}"
+    exit 1;
+  fi
+fi
 
+readonly STORFILE="saved.wmctrl"
+readonly TMPFILE="${STORFILE}.$(date +%s)"
+readonly DEFAULT_NUM_TO_KEEP="5"
 
-if [ "$1" == "save" ]; then
-  rm $STORFILE
-  wmctrl -l -G |while read window_id desktop_id x_offset y_offset width height junk; do
+if [[ "$1" == "save" ]]; then
+  wmctrl -l -G | \
+    while read window_id desktop_id x_offset y_offset width height junk; do
     offset=($(xwininfo -id $window_id |grep Relative | awk '{print $NF}'|xargs))
     # Although I tried to get precise position using xwininfo, I had to subtract
     # 15px from y position (Mint Cinnamon). May differ on your window manager.
     echo $window_id $desktop_id $((${x_offset}-${offset[0]})) \
-      $((${y_offset}-15-${offset[1]}-${offset[0]})) $width $height $junk >> $STORFILE
+      $((${y_offset}-15-${offset[1]}-${offset[0]})) $width $height $junk >> \
+          ${TMPFILE}
   done
-
-elif [ "$1" == "restore" ]; then
+  if [[ -f "${TMPFILE}" ]]; then
+    # ignore the window title when comparing, and any ordering differences.
+    if diff <(cut -d\  -f1-6 "${STORFILE}" | sort) \
+            <(cut -d\  -f1-6 "${TMPFILE}" | sort) >& /dev/null; then
+      rm "${TMPFILE}"
+    else
+      ln -s -f "${TMPFILE}" "${STORFILE}"
+    fi
+  fi
+elif [[ "$1" == "restore" ]]; then
+  readonly storfile="${2-${STORFILE}}"
   while read window_id desktop_id x_offset y_offset width height junk; do 
-    wmctrl -v -i -r $window_id -e 0,$x_offset,$y_offset,$width,$height 2>/dev/null
-  done < $STORFILE
+    wmctrl -v -i -r $window_id -e 0,$x_offset,$y_offset,$width,$height \
+      2>/dev/null
+  done < "${storfile}"
+elif [[ "$1" == "cleanup" ]]; then
+  # cleanup all but the most recent N (default 5) files
+  skip_count=${2-5}
+  while read -r file; do
+    ((--skip_count))
+    if [[ "${skip_count}" -lt 0 ]]; then
+      rm -v ${file}
+    fi
+  done <<< "$(ls -1t . --hide "${STORFILE}")"
+elif [[ "$1" == "show" ]]; then
+  ls -lt
+elif [[ "$1" == "diff" ]]; then
+  last_file="saved.wmctrl"
+  while read -r file; do
+    echo -n "${file} "
+    diff -y --suppress-common-lines <(cut -d\  -f1-6 "${last_file}" | sort) \
+      <(cut -d\  -f1-6 "${file}" | sort) | wc -l
+    last_file=${file}
+  done <<< "$(ls -1t . --hide "${STORFILE}")"
 else
-  echo "Missing argument. Usage:"
-  echo "    $0 <save|restore>"
+  readonly prog="$(basename $0)"
+  cat <<EOM
+Usage: ${prog} <save|show|diff>
+       ${prog} restore [filename (default "${STORFILE}"]
+       ${prog} cleanup [number-of-backups-to-keep (default ${DEFAULT_NUM_TO_KEEP})]
+EOM
   exit 127
 fi
-
